efbcfd84b94b791e879c46be75c312c9e09f661c
maxin <mx1759808857@163.com>
2018-04-19 21:22:20 +0800

    网络连接，动画播放

---
+++ Component/Assets/Scripts/Animate.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Animate : MonoBehaviour {
+    public Animation mAnimation;
+
+    // Use this for initialization  
+    void Start()
+    {
+        GameObject a = GameObject.Find("Cube");
+        mAnimation = a.GetComponent<Animator>().GetComponent<Animation>();
+        Debug.Log(mAnimation);
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (mAnimation.isPlaying)
+            {
+                mAnimation.Stop();
+            }
+            else
+            {
+                mAnimation.Play();
+            }
+        }
+    }
+
+
+    // Update is called once per frame  */
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (mAnimation.isPlaying)
+            {
+                mAnimation.Stop();
+            }
+            else
+            {
+                mAnimation.Play();
+            }
+        }
+    }
+
+    
+}
