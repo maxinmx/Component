d4c1bd02cf9b287d7c174e0197c0feea94aeb11a
maxin <mx1759808857@163.com>
2018-04-20 17:04:40 +0800

    动画的切换播放

--- Component/Assets/Scripts/Animate.cs
+++ Component/Assets/Scripts/Animate.cs
@@ -1,27 +1,26 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Text.RegularExpressions;
 
 public class Animate : MonoBehaviour {
     public Animation mAnimation;
+    public string receiveInfo = "<Test id='Cube1' operation='动画' value='play'></Test>";
+
+    private string execId;//与输入信息匹配的正则表达式确定id值
+    private string execOp;//确定操作
+    private string execValue;//确定操作的值
+    private string Id;//匹配后的id值
+    private string Op;//匹配后的操作
+    private string Value;//匹配后的值
+    private AnimationClip aniClip;
 
     // Use this for initialization  
     void Start()
     {
         GameObject a = GameObject.Find("Cube");
-        mAnimation = a.GetComponent<Animator>().GetComponent<Animation>();
-        Debug.Log(mAnimation);
-        if (Input.GetMouseButtonDown(0))
-        {
-            if (mAnimation.isPlaying)
-            {
-                mAnimation.Stop();
-            }
-            else
-            {
-                mAnimation.Play();
-            }
-        }
+        mAnimation = a.GetComponent<Animation>();
+        Analyze();
     }
 
 
@@ -30,16 +29,60 @@ public class Animate : MonoBehaviour {
     {
         if (Input.GetMouseButtonDown(0))
         {
-            if (mAnimation.isPlaying)
-            {
-                mAnimation.Stop();
-            }
-            else
-            {
-                mAnimation.Play();
-            }
+            Result(Id, Value);
         }
     }
 
-    
+    //得出id号和操作的名称及操作值
+    public void Analyze()
+    {
+        execId = @"<.*\sid='(.*)'\soperation.*>.*<.*>";
+        execOp = @"<.*\soperation='(.*)'\svalue.*>.*<.*>";
+        execValue = @"<.*\svalue='(.*)'>.*<.*>";
+        Id = Regex.Match(receiveInfo, execId).Groups[1].Value;
+        Op = Regex.Match(receiveInfo, execOp).Groups[1].Value;
+        Value = Regex.Match(receiveInfo, execValue).Groups[1].Value;
+    }
+
+    //判断操作名称以及将会执行的操作
+    public void Result(string id,string value)
+    {
+        if (value == "play")
+        {
+            setAnimationClipName(id);
+            mAnimation.Play();
+        }
+        else
+        {
+            setAnimationClipName(id);
+            mAnimation.Stop();
+        }
+    }
+
+
+    //获取当前正在播放的动画片段的名称
+    public string GetAnimationClipName()
+    {
+        foreach (AnimationState a in mAnimation)
+        {
+            if (mAnimation.IsPlaying(a.name))
+            {
+                return a.name;
+            }
+        }
+        return null;
+    }
+
+
+    //设置想要播放的动画
+    public void setAnimationClipName(string name)
+    {
+        if (GetAnimationClipName() != name)
+        {
+            aniClip = mAnimation.GetClip(name);
+            Debug.Log(mAnimation.GetClip(name));
+            mAnimation.clip = aniClip;
+        }
+        
+    }
 }
---
+++ Component/Assets/Scripts/Xml.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Xml;
+using System.Text.RegularExpressions;
+
+public class Xml : MonoBehaviour {
+    public string receiveInfo = "<Test id='1'>播放</Test>";
+
+    private string execId;//与输入信息匹配的正则表达式确定id值
+    private string execInfo;//确定信息值
+    private string Id;//匹配后的id值
+    private string Info;//匹配后的信息值
+
+    private Animation mAnimation;
+    // Use this for initialization
+    void Start () {
+        mAnimation = GameObject.Find("Cube").GetComponent<Animator>().GetComponent<Animation>();
+
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void Analyze()
+    {
+        execId = @"<.*\sid='(.*)'>.*<.*>";
+        execInfo =@">(.*)</";
+        Id = Regex.Match(receiveInfo, execId).Groups[1].Value;
+        Info = Regex.Match(receiveInfo, execInfo).Groups[1].Value;
+
+
+
+
+
+    }
+}
